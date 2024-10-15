34f48a52c952615e24e641c73aa2a5811231a08d
Rian Ramdin <rian-tristen@hotmail.com>
2024-10-15 02:02:35 -0400

    removed imports and views

--- App/views/course.py
+++ App/views/course.py
@@ -5,9 +5,6 @@ from .index import index_views
 
 from App.controllers import (
     create_course,
-    get_course_by_name,
-    get_course_by_code,
-    get_course,
     get_all_courses_json,
     update_course,
     get_course_json,
@@ -77,58 +74,3 @@ def edit_course():
 def get_all_courses_view():
     courses = get_all_courses_json()
     return jsonify(courses), 200
-
-
-# Route to get a course by course code
-@course_views.route('/code/<string:course_code>', methods=['GET'])
-def get_course_by_code_view(course_code):
-    course = get_course_by_code(course_code)
-    if not course:
-        return jsonify({'message': 'Course not found.'}), 404
-    return jsonify(course.get_json()), 200
-
-
-# Route to get a course by name
-@course_views.route('/name/<string:course_name>', methods=['GET'])
-def get_course_by_name_view(course_name):
-    course = get_course_by_name(course_name)
-    if not course:
-        return jsonify({'message': 'Course not found.'}), 404
-    return jsonify(course.get_json()), 200
-
-
-# Route to update a course
-@course_views.route('/<int:course_id>', methods=['PATCH'])
-def update_course_view(course_id):
-    data = request.json
-    attribute = data.get('attribute')
-    content = data.get('content')
-
-    if not attribute or not content:
-        return jsonify({'message': 'Attribute and content are required for update.'}), 400
-
-    updated_course = update_course(course_id, attribute, content)
-    if updated_course:
-        return jsonify({'message': 'Course updated successfully.'}), 200
-    else:
-        return jsonify({'message': 'Course not found or update failed.'}), 404
-
-
-# Route to get a single course by ID
-@course_views.route('/<int:course_id>', methods=['GET'])
-def get_course_view(course_id):
-    course = get_course(course_id)
-    if not course:
-        return jsonify({'message': 'Course not found.'}), 404
-    return jsonify(course.get_json()), 200
-
-
-# Route to delete a course by ID
-@course_views.route('/<int:course_id>', methods=['DELETE'])
-def delete_course_view(course_id):
-    course = get_course(course_id)
-    if course:
-        course.delete()
-        return jsonify({'message': 'Course deleted successfully.'}), 200
-    else:
-        return jsonify({'message': 'Course not found.'}), 404
